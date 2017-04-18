15a63950db358d9c7dcbbacc14474881b0515152
Titogelo <titogelo@gmail.com>
2017-04-18 07:46:51 +0200

    added redirect URL. token extraction does not work

--- App/containers/DojoIblMobile.js
+++ App/containers/DojoIblMobile.js
@@ -7,7 +7,7 @@ import {
   Linking
 } from 'react-native';
 
-var shittyQs = require('shitty-qs')
+// var shittyQs = require('shitty-qs')
 
 export default class DojoIblMobile extends Component {
   constructor(props) {
@@ -25,16 +25,63 @@ export default class DojoIblMobile extends Component {
     function urlHandler(event) {
 
       console.log(event.url);
-      var [, query_string] = event.url.match(/[^&?]*?=[^&?]*/)
-      var query = shittyQs(query_string)
-      console.log(query);
-      console.log(query_string);
 
-      if (state === query.state) {
-        callback(null, query.access_token, query.uid)
-      } else {
-        callback(new Error('Oauth2 security error'))
-      }
+      var requestToken = (event.url).split("code=")[1];
+      console.log(requestToken);
+
+      fetch('https://wespot-arlearn.appspot.com/oauth/token?client_id=wespotClientId&redirect_uri=http://localhost/callback&client_secret=wespotClientSecret&code=' + requestToken, {
+          method: 'post',
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+          }
+        })
+        .then(function (data) {
+            console.log('Request succeeded with JSON response', data);
+        })
+        .catch(function (error) {
+          console.log('Request failed', error);
+        });
+
+      // fetch('https://wespot-arlearn.appspot.com/oauth/token', {
+      //   method: 'POST',
+      //   data:{
+      //     client_id: 'wespotClientId',
+      //     redirect_uri: 'https://localhost/callback',
+      //     client_secret: 'wespotClientSecret',
+      //     grant_type: 'authorization_code',
+      //     code: 'requestToken',
+      //   }
+      // }).then((data) => {
+      //   console.log(data);
+      //   self.state.url = "JOLA"
+      // })
+      // .catch((error) => {
+      //   console.error(error);
+      // });
+
+      //
+      // $http({method: "post", url: "http://wespot-arlearn.appspot.com/oauth/token", data: "client_id=wespotClientId&redirect_uri=http://localhost/callback&client_secret=wespotClientSecret&grant_type=authorization_code&code=" + requestToken })
+      //  .success(function(data) {
+      //    alert(data.access_token)
+      //    accessToken = data.access_token;
+      //    $location.path("/app/inquiries");
+      //  })
+      //  .error(function(data, status) {
+      //    alert(data)
+      //
+      //    console.log("ERROR: " + data);
+      //  });
+
+      // var [, query_string] = event.url.match(/[^&?]*?=[^&?]*/)
+      // var query = shittyQs(query_string)
+      // console.log(query);
+      // console.log(query_string);
+      //
+      // if (state === query.state) {
+      //   callback(null, query.access_token, query.uid)
+      // } else {
+      //   callback(new Error('Oauth2 security error'))
+      // }
 
       Linking.removeEventListener('url', urlHandler);
     }
