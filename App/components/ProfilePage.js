0e5dce98baa08b43029a293d703ae2bb7d72c434
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-04-23 19:13:11 +0200

    Implemented drawer

--- App/components/ProfilePage.js
+++ App/components/ProfilePage.js
@@ -10,20 +10,45 @@ import {
 import { globalStyles } from '../styles/globalStyles';
 import { colors } from '../styles/colors';
 import { sizes } from '../styles/sizes';
+import Drawer from 'react-native-drawer';
 import ProfileHeader from './ProfileHeader';
 
 export default class ProfilePage extends Component {
+  closeControlPanel = () => {
+    this._drawer.close();
+  };
+
+  openControlPanel = () => {
+    this._drawer.open();
+  };
+
   render() {
     return (
-      <ScrollView style={{flex: 1, backgroundColor: '#2F4050'}}>
-        <ProfileHeader tokens={this.props.tokens} />
-        <TouchableHighlight onPress={this.props.logout}>
-          <Text style={styles.logoutBtn}>Logout</Text>
-        </TouchableHighlight>
-        <Text style={styles.tasksTitle}>Upcoming activities</Text>
-      </ScrollView>
+      <Drawer
+        ref={(ref) => this._drawer = ref}
+        type='static'
+        content={<View><Text>woopwoop</Text></View>}
+        openDrawerOffset={0.2}
+        styles={drawerStyles}
+        tapToClose={true}
+        tweenHandler={Drawer.tweenPresets.parallax}
+        closedDrawerOffset={-3}
+        >
+        <ScrollView style={{flex: 1, backgroundColor: '#2F4050'}}>
+          <ProfileHeader tokens={this.props.tokens} />
+          <TouchableHighlight onPress={this.openControlPanel}>
+            <Text style={styles.logoutBtn}>Logout</Text>
+          </TouchableHighlight>
+          <Text style={styles.tasksTitle}>Upcoming activities</Text>
+        </ScrollView>
+      </Drawer>
     );
   }
+}
+
+const drawerStyles = {
+  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+  main: {paddingLeft: 3},
 }
 
 const styles = StyleSheet.create({
