b8ec859dede762ec472366ca820f29ed76ece176
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-05-22 15:49:41 +0200

    Added chat layout

--- App/screens/Chat.js
+++ App/screens/Chat.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
   Text,
   View
 } from 'react-native';
@@ -8,10 +9,12 @@ import { colors } from '../styles/colors';
 import { sizes } from '../styles/sizes';
 import Utils from '../lib/Utils';
 import RequestUtils from '../lib/RequestUtils';
+import InvertibleScrollView from 'react-native-invertible-scroll-view';
+import ChatForm from '../components/ChatForm';
 
 export default class Chat extends Component {
   static navigationOptions = ({ navigation }) => ({
-    title: navigation.state.params.groupName
+    title: `${navigation.state.params.groupName} chat`
   });
 
   runId = this.props.navigation.state.params.runId;
@@ -19,7 +22,47 @@ export default class Chat extends Component {
 
   render() {
     return (
-      <Text>Ma boi</Text>
+      <InvertibleScrollView
+        inverted
+        style={styles.container}
+      >
+        <ChatForm />
+        <View style={{flex: 1}}>
+          {new Array(1000).fill('message').map((message) => {
+            const randomNum = Math.round(Math.random() * 10);
+            const style = randomNum == 5
+                          ? StyleSheet.flatten([styles.message, styles.currentUserMessage])
+                          : styles.message;
+
+            const textStyle = randomNum == 5 ? { color: colors.textColor } : {};
+
+            return (
+              <View style={style}>
+                <Text style={textStyle}>{message}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </InvertibleScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: sizes.offset / 2,
+    paddingRight: sizes.offset / 2,
+    backgroundColor: colors.backgroundColor
+  },
+  message: {
+    marginBottom: sizes.offset / 2,
+    padding: sizes.offset / 2,
+    width: sizes.window.width * 0.7,
+    backgroundColor: colors.textColor,
+    borderRadius: sizes.offset / 2
+  },
+  currentUserMessage: {
+    marginLeft: sizes.window.width * 0.3 - sizes.offset,
+    backgroundColor: colors.secondaryTextColor
+  }
+});
