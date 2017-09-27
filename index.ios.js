49f95c74b3ad67310d183097ff58b70faf0c4d6c
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-09-27 13:31:18 +0200

    Recreated project

--- index.ios.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import { routes } from './App/routes';
-
-const App = StackNavigator(routes, { initialRouteName: 'Home', headerMode: 'screen' });
-
-AppRegistry.registerComponent('DojoIblMobile', () => App);
