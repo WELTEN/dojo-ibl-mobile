38e2c6672cce44afb7e9519d83a4c0b834484cce
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-09-28 11:33:44 +0200

    Added sign in with Google & firebase auth

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
 import Home from './components/Home';
+import firebaseConfig from './firebaseConfig';
+import * as firebase from 'firebase';
+
+firebase.initializeApp(firebaseConfig);
 
 const App = StackNavigator({
   Home: { screen: Home }
--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableHighlight } from 'react-native';
 import glamorous from 'glamorous-native';
-import { GoogleSignin } from 'react-native-google-signin';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+import * as firebase from 'firebase';
 
 const Container = glamorous.view({
   flex: 1,
@@ -15,14 +16,88 @@ export default class Home extends Component {
     title: 'Home'
   };
 
-  render = () => (
-    <Container>
-      <Text>DojoIBL</Text>
-      <GoogleSigninButton
-        size={GoogleSigninButton.Size.Icon}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={() => alert('kes')}
-      />
-    </Container>
-  );
+  state = {
+    loggedIn: false,
+    loading: true
+  };
+
+  componentWillMount() {
+    GoogleSignin.hasPlayServices({ autoResolve: true }).then(() => {
+      GoogleSignin.configure({
+        iosClientId: '518897628174-u6ufotog815h8nm5lbibm8inictcvsh2.apps.googleusercontent.com',
+        webClientId: '518897628174-u6ufotog815h8nm5lbibm8inictcvsh2.apps.googleusercontent.com'
+      });
+      GoogleSignin.currentUserAsync().then(this.handleUser);
+    }).catch((err) => {
+      alert(`Play services error, ${err.code}, ${err.message}`);
+    });
+  }
+
+  handleUser = (user) => {
+    if (user) {
+      this.setState({ loggedIn: true, loading: false, user });
+      this.authUser(user);
+    } else {
+      this.setState({ loggedIn: false, loading: false, user: null });
+    }
+  }
+
+  authUser = (user) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      unsubscribe();
+      if (!this.isUserEqual(user, firebaseUser)) {
+        const credential = firebase.auth.GoogleAuthProvider.credential(user.idToken);
+
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          console.log(error);
+        });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    });
+  }
+
+  isUserEqual = (user, firebaseUser) => {
+    if (firebaseUser) {
+      const providerData = firebaseUser.providerData;
+      for (let i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+            providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  onLogin = () => {
+    GoogleSignin.signIn().then(this.handleUser);
+  }
+
+  onLogout = () => {
+    firebase.auth().signOut();
+    GoogleSignin.signOut().then(this.handleUser);
+  }
+
+  render() {
+    if (this.state.loggedIn) {
+      return (
+        <Container>
+          <Text>{this.state.user.email}</Text>
+          <TouchableHighlight onPress={this.onLogout}>
+            <Text>Logout</Text>
+          </TouchableHighlight>
+        </Container>
+      );
+    } else {
+      return (
+        <Container>
+          <Text>DojoIBL</Text>
+          <TouchableHighlight onPress={this.onLogin}>
+            <Text>Login</Text>
+          </TouchableHighlight>
+        </Container>
+      );
+    }
+  }
 }
