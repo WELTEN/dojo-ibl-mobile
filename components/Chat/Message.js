640cac0acbdae1395ccb5269c081924f97a777d3
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-10-11 09:58:49 +0200

    Fixed #29 Finished chat

--- components/Chat/Message.js
+++ components/Chat/Message.js
@@ -6,38 +6,76 @@ import { getPictureUrl } from '../../lib/Text';
 import moment from 'moment';
 
 const Item = glamorous.view({
+  marginBottom: 16,
   marginLeft: 24,
   marginRight: 24,
   maxWidth: Dimensions.get('window').width / 10 * 6,
-  flexDirection: 'row'
 }, ({ isCurrentUser }) => {
-  if (isCurrentUser) return [{ alignSelf: 'flex-end' }];
+  if (isCurrentUser) return [{ marginBottom: 4, alignSelf: 'flex-end' }];
+});
+
+const Header = glamorous.view({
+  marginBottom: 4,
+  flexDirection: 'row'
 });
 
 const Picture = glamorous.image({
-  marginRight: 12,
-  width: 36,
-  height: 36,
-  borderRadius: 18,
+  marginRight: 8,
+  width: 22,
+  height: 22,
+  borderRadius: 11,
   alignSelf: 'baseline'
 });
 
+const Name = glamorous.text({
+  color: '#616161',
+  fontSize: 12,
+  lineHeight: 22
+});
+
+const ContentContainer = glamorous.view({
+  flexDirection: 'row'
+});
+
 const Content = glamorous.view({
-  backgroundColor: '#EEEEEE'
+  padding: 10,
+  backgroundColor: '#EEEEEE',
+  borderRadius: 28
 }, ({ isCurrentUser }) => {
   if (isCurrentUser) return [{ backgroundColor: '#2196F3' }];
 });
 
+const BodyText = glamorous.text({}, ({ isCurrentUser }) => {
+  if (isCurrentUser) return [{ color: 'white' }];
+});
+
+const Date = glamorous.text({
+  marginTop: 4,
+  color: '#BDBDBD',
+  fontSize: 12
+});
+
 const Message = ({ isCurrentUser, message }) => (
   <Item isCurrentUser={isCurrentUser}>
+    <Header>
+      {!isCurrentUser &&
+        <Picture source={{ uri: getPictureUrl(message.picture) }} />
+      }
+      {!isCurrentUser &&
+        <Name>{message.name}</Name>
+      }
+    </Header>
+    <ContentContainer>
+      <Content isCurrentUser={isCurrentUser}>
+        <BodyText isCurrentUser={isCurrentUser}>
+          {message.body}
+        </BodyText>
+      </Content>
+      <View style={{ flex: 1 }} />
+    </ContentContainer>
     {!isCurrentUser &&
-      <Picture source={{ uri: getPictureUrl(message.picture) }} />
+      <Date>{moment(message.date).calendar()}</Date>
     }
-    <Content isCurrentUser={isCurrentUser}>
-      <Text>{message.body}</Text>
-      <Text>{moment(message.date).calendar()}</Text>
-      <Text>{message.name}</Text>
-    </Content>
   </Item>
 );
 
